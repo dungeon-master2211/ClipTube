8e3b759f7668468a5b1737675fbf03ae833a85bc
harshit_2211 <harshit.sri2211@gmail.com>
2020-10-08 16:56:50 +0530

    ClipTube-Flask Python ,HTML, CSS and Bootstrap

---
+++ main.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 19 10:30:18 2020
+
+@author: HARSHIT
+"""
+import pafy
+from flask import Flask,render_template,request
+app=Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def cliptube():
+    
+    if request.method=='POST':
+        url=request.form.get('link')
+        video = pafy.new(url)
+        
+        if request.form.get('video'):
+            best=video.getbest()
+            
+            links=best.url
+            title=best.title
+            extension=best.extension
+            s=best.get_filesize()
+            size=s//1000000
+            return render_template('down.html',links=links,title=title,extension=extension,size=size)
+        if request.form.get('audio'):    
+            audio=video.getbestaudio()
+            links=audio.url
+            title=audio.title
+            extension=audio.extension
+            s=audio.get_filesize()
+            size=s//1000000
+            return render_template('down.html',links=links,title=title,extension=extension,size=size)
+            #audio.download(filepath="C:\\Users\\HARSHIT\\youtube_downloader\\static")'''
+            
+    return render_template('index.html')
+
+@app.route('/down')
+def down():
+    return render_template('down.html')
+
+app.run(port=int(777),debug=True)
