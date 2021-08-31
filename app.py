d91ad85038174f2bdca5db1961af29538fd6b5f9
harshit_srivastava <harshit.sri2211@gmail.com>
2021-08-31 18:08:04 +0530

    fix input field

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ Created on Sat Sep 19 10:30:18 2020
 
 @author: HARSHIT
 """
+import os
 import pafy
 from flask import Flask,render_template,request
 app=Flask(__name__)
@@ -14,26 +15,29 @@ def cliptube():
     if request.method=='POST':
         url=request.form.get('link')
         video = pafy.new(url)
-        
-        if request.form.get('video'):
-            best=video.getbest()
-            
-            links=best.url
-            title=best.title
-            extension=best.extension
-            s=best.get_filesize()
-            size=s//1000000
-            return render_template('down.html',links=links,title=title,extension=extension,size=size)
-        if request.form.get('audio'):    
-            audio=video.getbestaudio()
-            links=audio.url
-            title=audio.title
-            extension=audio.extension
-            s=audio.get_filesize()
-            size=s//1000000
-            return render_template('down.html',links=links,title=title,extension=extension,size=size)
-            #audio.download(filepath="C:\\Users\\HARSHIT\\youtube_downloader\\static")'''
-            
+        try:
+
+            if request.form.get('video'):
+                best=video.getbest()
+                filename=best.download()
+                links=best.url
+                title=best.title
+                extension=best.extension
+                s=best.get_filesize()
+                size=s//1000000
+                return render_template('down.html',links=links,title=title,extension=extension,size=size)
+                
+            if request.form.get('audio'):    
+                audio=video.getbestaudio()
+                links=audio.url
+                title=audio.title
+                extension=audio.extension
+                s=audio.get_filesize()
+                size=s//1000000
+                return render_template('down.html',links=links,title=title,extension=extension,size=size)
+                #audio.download(filepath="C:\\Users\\HARSHIT\\youtube_downloader\\static")'''
+        except ValueError:
+            return "Incorrect URL"    
     return render_template('index.html')
 
 @app.route('/down')
